35ea1451cffff0fca3c9b5eeb6a961bd237f6275
Matilda578 <101889190@student.swin.edu.au>
2020-04-02 19:30:47 +1000

    made a user input

--- Program.cs
+++ Program.cs
@@ -6,12 +6,19 @@ namespace Nested
     {
         static void Main(string[] args)
         {
+           Console.WriteLine("How many xs do you want?!??!"); //asking for user input
+           string usersad = Console.ReadLine(); //getting input as a string
+          //create an integer variable
+          int BLAH; 
+          //make blah mean what the user input but as an integer
+         BLAH = Int32.Parse(usersad);
+                     
            int totalloops = 5;  
          int currentloop, nestedcurrentloop ;
          for (currentloop = 1; currentloop <= totalloops; currentloop++)  
          {  
            
-            for (nestedcurrentloop = 5; nestedcurrentloop >= currentloop; nestedcurrentloop--)  
+            for (nestedcurrentloop = BLAH; nestedcurrentloop >= currentloop; nestedcurrentloop--)  
             {  
                Console.Write("*");  
             }  
